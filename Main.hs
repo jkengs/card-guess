b80f036524fb7133657c39dd5ae661d6d87cf57a
jkengs <107338480+jkengs@users.noreply.github.com>
2023-06-01 15:02:00 +0800

    feat: add main game loop

---
+++ Main.hs
@@ -0,0 +1,19 @@
+module Main(main) where
+import Guess
+
+main :: IO ()
+main = do
+    putStrLn "- Welcome to the Card Guessing Game!"
+    putStrLn "- Enter the cards (answer) in the format \"4C 3H\""
+    putStrLn "- Type 'exit' if you wish to leave the game"
+    gameLoop
+
+gameLoop :: IO ()
+gameLoop = do
+    putStr "- "
+    answer <- getLine
+    if answer == "exit" then
+        putStrLn "Exiting the game. Goodbye!"
+    else do
+        guess answer
+        gameLoop
